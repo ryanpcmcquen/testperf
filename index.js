c983df0c1d4a14f4eb84b067491f4d10fece53df
Ryan P.C. McQuen <ryan.q@linux.com>
2016-08-08 19:24:35 -0700

    Switch from Date.now() to performance.now().
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*! testperf v1.0.1 by ryanpcmcquen */
+/*! testperf v1.1.0 by ryanpcmcquen */
 
 /*global module*/
 /*jshint esversion:6*/
@@ -8,12 +8,12 @@ const testPerf = function (name, fn) {
   const i = [];
   i.length = 500500;
   i.fill(0);
-  const start = Date.now();
+  const start = performance.now();
   i.map(() => {
     fn.apply(fn, args);
   });
-  const end = Date.now();
-  console.log(name + " took: " + (end - start));
+  const end = performance.now();
+  console.log(name + " took: " + (end - start) + " milliseconds.");
 };
 
 module.exports = testPerf;
