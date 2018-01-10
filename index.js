bf155d2c81d1469728a2e17a21aff6226a80916c
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2018-01-10 14:29:38 -0800

    Code cleanup.

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
-/*! testperf v2.0.3 by ryanpcmcquen */
+/*! testperf v2.0.4 by ryanpcmcquen */
 // @flow
-'use strict';
-let now;
+"use strict"
+let now
 try {
-  (window.performance) && (now = () => {
-    return window.performance.now();
-  });
+    window.performance &&
+        (now = () => {
+            return window.performance.now()
+        })
 } catch (ignore) {
-  now = (diff) => {
-    return (diff) ? process.hrtime(diff) : process.hrtime();
-  };
+    now = diff => {
+        return diff ? process.hrtime(diff) : process.hrtime()
+    }
 }
-const timediff = (end /*:any*/ , start) => {
-  // Format `process.hrtime()` to match
-  // `performance.now()` (milliseconds).
-  return (typeof start === 'number') ? (end - start) : ((end[0] * 1e3) + (end[1] / 1e6));
-};
+const timediff = (end /*:any*/, start) => {
+    // Format `process.hrtime()` to match
+    // `performance.now()` (milliseconds).
+    return typeof start === "number" ? end - start : end[0] * 1e3 + end[1] / 1e6
+}
 
-const testPerf = function (name /*:String*/ , fn /*:Function*/ ) {
-  const args = Array.prototype.slice.call(arguments, 2);
-  const i = [];
-  i.length = 500500;
-  i.fill(0);
-  const start = now();
-  i.map(() => {
-    fn.apply(fn, args);
-  });
-  const end = now(start);
-  // Use commas to avoid confusing type inference.
-  console.log('// =>', name, 'took:', (timediff(end, start)), 'milliseconds.');
-};
+const testPerf = (name /*:String*/, fn /*:Function*/, ...args) => {
+    const start = now()
+    const test = Array(500500)
+        .fill()
+        .map(() => fn.apply(fn, args))
+    const end = now(start)
+    // Use commas to avoid confusing type inference.
+    console.log("// =>", name, "took:", timediff(end, start), "milliseconds.")
+}
 
-module.exports = testPerf;
+module.exports = testPerf
